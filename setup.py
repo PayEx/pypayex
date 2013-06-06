74546d1d24b9ed5e0bde7f7fa5a2116febd8180e
Jon Lønne <jon@funkbit.no>
2013-06-06 15:54:15 +0200

    Cleaned up setup.py and license.

--- setup.py
+++ setup.py
@@ -25,18 +25,19 @@ install_requires = [
 if sys.version_info < (2, 7):
     install_requires.append('ordereddict')
 
-setup(name='pypayex',
-      version=__version__,
-      description='PayEx API wrapper',
-      long_description=open('README.md').read(),
-      author='Funkbit AS',
-      author_email='post@funkbit.no',
-      url='https://github.com/funkbit/pypayex',
-      packages=['payex'],
-      license='BSD',
-      install_requires=install_requires,
-      test_suite='tests',
-      classifiers = (
+setup(
+    name='pypayex',
+    version=__version__,
+    description='PayEx API wrapper',
+    long_description=open('README.md').read(),
+    author='Funkbit',
+    author_email='post@funkbit.no',
+    url='https://github.com/funkbit/pypayex',
+    packages=['payex'],
+    license='BSD',
+    install_requires=install_requires,
+    test_suite='tests',
+    classifiers=(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -44,5 +45,5 @@ setup(name='pypayex',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        )
-     )
+    )
+)
